92e5cb8ba7a77302def2c5f5ba3c269ab23bd08b
we0091234 <447587096@qq.com>
2022-12-08 21:51:30 +0800

    demo.sh

--- demo.sh
+++ demo.sh
@@ -1,1 +1,2 @@
-python detect_plate.py --detect_model runs/train/exp22/weights/last.pt --rec_model /mnt/Gpan/Mydata/pytorchPorject/CRNN/newCrnn/crnn_plate_recognition/output/360CC/crnn/2022-12-04-08-40/checkpoints/checkpoint_97_acc_0.9712.pth --image_path mytest --img_size 384
+python detect_plate.py --detect_model runs/train/exp26/weights/last.pt  --image_path imgs --img_size 640
+# runs/train/exp26/weights/last.pt
